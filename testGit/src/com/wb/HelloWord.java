06689e8705aaddf5cf86e7522a93e933705317d8
zhangsan <2679019526@qq.com>
2022-05-18 11:47:25 +0800

    定义了一个私有属性

--- testGit/src/com/wb/HelloWord.java
+++ testGit/src/com/wb/HelloWord.java
@@ -3,6 +3,7 @@ package com.wb;
 public class HelloWord {
     private int age;
     private String name;
+    private String address;
     public static void main(String[] args) {
         System.out.println("helloWorld!");
     }
